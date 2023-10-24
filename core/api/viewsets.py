8fe55f86382ed74fbb825970bcd24a1c6d691979
lucasops96 <lucasbanana96@gmail.com>
2023-10-24 09:24:54 -0300

    sobrescrevendo actions

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-
+from rest_framework.decorators import action
 from core.models import PontoTuristico
 
 from .serializers import PontoTuristicoSerializer
@@ -16,6 +16,28 @@ class PontoTuristicoViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
     
-    # def list(self, request, *args, **kwargs):
-    #     return Response({'teste':123})
+    def list(self, request, *args, **kwargs):
+        return Response({'teste':123})
     
+    def create(self,request, *args, **kwargs):
+        return Response({'Hello':request.data['nome']})
+    
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass 
+    
+    def partial_update(self, request, *args, **kwargs):
+        pass
+
+    @action(methods=['get'],detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+    
+    @action(methods=['get'],detail=False)
+    def teste(self,request):
+        pass
